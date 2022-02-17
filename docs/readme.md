1914776624d572c44964d039de4dad6972cadc12
Devin Harris <49620674+DevinStormHarris@users.noreply.github.com>
2022-02-17 08:50:07 -0500

    change frontend from angular to blazor wasm

--- docs/readme.md
+++ docs/readme.md
@@ -1,10 +1,2 @@
 # Paper
-This is a note taking application and a practice project intended to explore full-stack development.
-
-### Technologies
-- C# 
-- .NET6
-- ASP.NET Core
-- Angular
-- TypeScript
-- Bootstrap
+This is a note taking application created as a practice project.
--- src/Paper.Web/Program.cs
+++ src/Paper.Web/Program.cs
@@ -1,28 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Paper.Web;
 
-// Add services to the container.
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");
-;
-
-app.Run();
+await builder.Build().RunAsync();
